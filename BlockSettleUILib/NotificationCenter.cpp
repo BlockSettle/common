d39ec3925eade965714d01b48ce8228dcb6971d1
Jack L <zheka_93@ukr.net>
2019-04-18 20:44:51 +0300

    Fixed indent intervals.

--- BlockSettleUILib/NotificationCenter.cpp
+++ BlockSettleUILib/NotificationCenter.cpp
@@ -272,10 +272,10 @@ void NotificationTrayIconResponder::messageClicked()
    }
    else if (newChatMessage_) {
       if (!newChatId_.isNull() && globalInstance != NULL) {
-			emit globalInstance->newChatMessageClick(newChatId_);
+         emit globalInstance->newChatMessageClick(newChatId_);
          mainWinUi_->tabWidget->setCurrentWidget(mainWinUi_->widgetChat);
          mainWinUi_->tabWidget->activateWindow();
-		}
+      }
    }
 }
 
